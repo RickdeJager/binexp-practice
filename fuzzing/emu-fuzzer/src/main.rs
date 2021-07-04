eb4506471377b334ba33f7509c5bbc83507237a7
Rick de Jager <rickdejager99@gmail.com>
2021-07-04 14:19:03 +0200

    Added forking

--- fuzzing/emu-fuzzer/src/main.rs
+++ fuzzing/emu-fuzzer/src/main.rs
@@ -5,6 +5,12 @@ const PERM_EXEC : u8 = 1 << 2;
 // Read after write
 const PERM_RAW  : u8 = 1 << 3;
 
+/// Block size used to track dirty blocks of memory. This value can be tweaked to optimise
+/// perf for your specific target.
+/// Setting this to a larger value causes fewer, large memcpy's to occur, setting
+/// this to a lower value causes more, smaller memcpy's to occur.
+const DIRTY_BLOCK_SIZE: usize = 4096;
+
 
 /// Permission flags to track the state of bytes in memory.
 #[repr(transparent)]
@@ -25,6 +31,12 @@ struct Mmu {
     /// Permission bytes for the corresponding byte in `memory`
     permissions: Vec<Perm>,
 
+    /// Tracks blocks indices of memory that are dirty.
+    dirty: Vec<usize>,
+
+    /// Tracks which parts of memory have been dirtied.
+    dirty_bitmap: Vec<u64>,
+
     /// Current base address of the next allocation to perform
     cur_alloc: VirtAddr,
 }
@@ -33,10 +45,45 @@ impl Mmu {
     /// Create a new memory space of size `size`
     fn new(size: usize) -> Self {
         Mmu {
-            memory     : vec![0; size],
-            permissions: vec![Perm(0); size],
-            cur_alloc  : VirtAddr(0x10000),
+            memory      : vec![0; size],
+            permissions : vec![Perm(0); size],
+            dirty       : Vec::with_capacity(size / DIRTY_BLOCK_SIZE + 1),
+            dirty_bitmap: vec![0u64; size / DIRTY_BLOCK_SIZE / 64 + 1],
+            cur_alloc   : VirtAddr(0x10000),
         }
+    }
+
+    // Fork from an existing Mmu
+    pub fn fork(&self) -> Self {
+        let size = self.memory.len();
+
+        Mmu {
+            memory      : self.memory.clone(),
+            permissions : self.permissions.clone(),
+            // Keep the dirty bits clear.                          
+            dirty       : Vec::with_capacity(size / DIRTY_BLOCK_SIZE + 1),
+            dirty_bitmap: vec![0u64; size / DIRTY_BLOCK_SIZE / 64 + 1],
+            cur_alloc   : self.cur_alloc.clone(),
+        }
+    }
+
+    /// Restore the Mmu's memory to some original state. This will take all dirty blocks
+    /// in `self`, and replace them by the contents in the provided state.
+    pub fn reset(&mut self, other: &Mmu) {
+        for &block in &self.dirty {
+            let start =  block * DIRTY_BLOCK_SIZE;
+            let end   = (block + 1) * DIRTY_BLOCK_SIZE;
+
+            // Zero out the bitmap
+            self.dirty_bitmap[block / 64] = 0;
+
+            // Restore the memory and permissions
+            self.memory[start..end].copy_from_slice(&other.memory[start..end]);
+            self.permissions[start..end].copy_from_slice(&other.permissions[start..end]);
+        }
+
+        // Clear the old dirty list
+        self.dirty.clear();
     }
 
     /// Allocate a region of memory as RW.
@@ -100,6 +147,25 @@ impl Mmu {
             .get_mut(addr.0..addr.0.checked_add(buf.len())?)?
             .copy_from_slice(buf);
 
+        // Mark dirty blocks
+        let block_start = addr.0 / DIRTY_BLOCK_SIZE;
+        let block_end   = (addr.0 + buf.len()) / DIRTY_BLOCK_SIZE;
+
+        for block in block_start..=block_end {
+            // Determine the bitmap position of the dirty block
+            let idx = block_start / 64;
+            let bit = block_start % 64;
+
+            // Check if the block is not dirty
+            if self.dirty_bitmap[idx] & (1 << bit) == 0 {
+                // Block is not dirty yet, add it to the dirty list.
+                self.dirty.push(block);
+
+                // Update the dirty bitmap
+                self.dirty_bitmap[idx] |= 1 << bit;
+            }
+        }
+
         // If any bytes had the RAW bit set, mark everything we just wrote to as readable.
         if has_raw {
             perms
@@ -143,18 +209,28 @@ impl Emulator {
             memory: Mmu::new(size),
         }
     }
+
+    /// Fork the emulator into a new emulator, copying the current Mmu state.
+    pub fn fork(&self) -> Self {
+        Emulator {
+            memory: self.memory.fork(),
+        }
+    }
 }
 
 fn main() {
 
     let mut emu = Emulator::new(1024*1024);
+    let tmp = emu.memory.allocate(4).unwrap();
 
-    let mut tmp = emu.memory.allocate(4096).unwrap();
-    emu.memory.write_from(tmp, b"AAAABBBBCCCCDDDD").unwrap();
+    emu.memory.write_from(tmp, b"XXXX").unwrap();
 
-    let mut bytes = [0u8; 4];
-    emu.memory.read_into(tmp, &mut bytes).unwrap();
+    {
+        let mut forked = emu.fork();
+        for _ in 0..5_000_000 {
+            forked.memory.write_from(tmp, b"ABCD").unwrap();
+            forked.memory.reset(&emu.memory);
+        }
+    }
 
-    print!("{:x?} \n", bytes);
-    println!("Hello, world!");
 }
